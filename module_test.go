228a5dd1fe74581201211361516dc829b88f8590
Walker O'Brien <walkernobrien@gmail.com>
2025-06-12 03:35:57 -0500

    fix provider

--- module_test.go
+++ module_test.go
@@ -2,40 +2,60 @@ package test
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-var tfDir = "../"
+const authProviderBlock = `
+provider "github" {
+  owner = var.github_org
+  app_auth {
+    id              = var.github_app_id
+    installation_id = var.github_app_installation_id
+    pem_file        = file(var.github_app_private_key_path)
+  }
+}
+`
 
 func TestRepoModule(t *testing.T) {
-	input, err := os.ReadFile("./provider.tf")
+	providerFile := "provider.tf"
+
+	tmpDir, err := os.MkdirTemp("", "terratest-tmp")
 	if err != nil {
-		t.Fatalf("Failed to read providers.tf: %v", err)
+		t.Fatalf("Failed to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	originalContents, err := os.ReadFile(providerFile)
+	if err != nil {
+		t.Fatalf("Failed to read provider.tf: %v", err)
 	}
 
-	destFilePath := tfDir + "provider.tf"
-	if err = os.WriteFile(destFilePath, input, 0o644); err != nil {
-		t.Fatalf("Failed to write providers.tf to destination: %v", err)
+	tmpFile := filepath.Join(tmpDir,providerFile)
+	if err := os.WriteFile(tmpFile, originalContents, 0o644); err != nil {
+		t.Fatalf("Failed to write to temp provider.tf: %v", err)
 	}
 
-	if err := os.Remove("./provider.tf"); err != nil {
-		t.Fatalf("Failed to delete providers.tf: %v", err)
+	f, err := os.OpenFile(providerFile, os.O_APPEND|os.O_WRONLY, 0o644)
+	if err != nil {
+		t.Fatalf("Failed to open temp provider.tf for appending: %v", err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(authProviderBlock); err != nil {
+		t.Fatalf("Failed to append provider block: %v", err)
 	}
 
 	defer func() {
-		if err = os.WriteFile("./provider.tf", input, 0o644); err != nil {
-			t.Fatalf("Failed to write providers.tf to destination: %v", err)
-		}
-
-		if err := os.Remove(destFilePath); err != nil {
-			t.Fatalf("Failed to delete providers.tf: %v", err)
+		if err := os.WriteFile(providerFile, originalContents, 0o644); err != nil {
+			t.Fatalf("Failed to restore original provider.tf: %v", err)
 		}
 	}()
 
 	opts := &terraform.Options{
-		TerraformDir: tfDir,
+		TerraformDir: ".",
 		EnvVars:      map[string]string{},
 		Vars: map[string]interface{}{
 			"org_name":            "cloudwalk",
