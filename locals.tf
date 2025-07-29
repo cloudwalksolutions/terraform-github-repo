2d10e4f95f9f8dc82e74389e793218b28363eed2
Walker O'Brien <walkernobrien@gmail.com>
2025-07-29 14:09:34 -0400

    oops

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   name_prefix = length(local.name_items) > 1 ? join("-", slice(local.name_items, 0, length(local.name_items) - 1)) : var.name
 
   admin_project_prefix = var.admin_project_prefix != "" ? var.admin_project_prefix : var.state_bucket_prefix
-  admin_project_label = "${var.state_bucket_prefix}-admin"
+  admin_project_label = "${local.admin_project_prefix}-admin"
   admin_project_id    = "${local.name_prefix}-${local.admin_project_label}-project"
 
   workspace_project_id = var.workspace_project_id != "" ? var.workspace_project_id : local.admin_project_id
