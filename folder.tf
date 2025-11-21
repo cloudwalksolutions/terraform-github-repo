60cae830e997bd04f2d64b57f75d1c13ecaa0026
Walker O'Brien <walkernobrien@gmail.com>
2025-11-20 22:57:18 -0500

    new folder

--- folder.tf
+++ folder.tf
@@ -2,7 +2,7 @@
 module "gcp_folder" {
   count = var.create_gcp_folder ? 1 : 0
 
-  source = "git::https://github.com/cloudwalksolutions/terraform-google-folder.git?ref=0.0.25"
+  source = "git::https://github.com/cloudwalksolutions/terraform-google-folder.git?ref=0.0.26"
 
   parent_folder_id = var.gcp_parent_folder_id
   folder_name      = var.gcp_folder_name != "" ? var.gcp_folder_name : var.name
